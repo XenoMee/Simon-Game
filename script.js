d372e1eaa6cfd0aab5a5b1f1c761347d90818f96
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-27 16:52:08 +0200

    create function to generate a random color and push it to game pattern array

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 // TODO: Store user's choices
 let colorChoices = ["green", "red", "yellow", "blue"];
 let gamePattern = [];
-let userChoice = [];
+let userPattern = [];
 
-// const randomGeneratedColor = function () {
-//   let randomColor = Math.floor(Math.random() * 4);
-// };
+// Generates a random color from the colorChoices array and adds that color to
+// the game pattern array.
+const randomGeneratedColor = function () {
+  let randomColor = Math.floor(Math.random() * 4);
+  gamePattern.push(colorChoices[randomColor]);
+};

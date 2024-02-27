9a95516c16d7d86240672b85bb1c8d5d5a092f3f
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-27 17:21:34 +0200

    create function to play a sound based on the random generated color

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // TODO: Start the game by pressing any key
 // TODO: Create game pattern sequence
-// TODO: Store user's choices
+// TODO: Create user pattern sequence
+// TODO: Add sound and animation to the randomly picked buttons
 let colorChoices = ["green", "red", "yellow", "blue"];
 let gamePattern = [];
 let userPattern = [];
@@ -10,4 +11,31 @@ let userPattern = [];
 const randomGeneratedColor = function () {
   let randomColor = Math.floor(Math.random() * 4);
   gamePattern.push(colorChoices[randomColor]);
+};
+
+const playSound = function (currentColor) {
+  switch (currentColor) {
+    case "green":
+      const greenSound = new Audio("./sounds/green.mp3");
+      greenSound.play();
+      break;
+
+    case "red":
+      const redSound = new Audio("./sounds/red.mp3");
+      redSound.play();
+      break;
+
+    case "yellow":
+      const yellowSound = new Audio("./sounds/yellow.mp3");
+      yellowSound.play();
+      break;
+
+    case "blue":
+      const blueSound = new Audio("./sounds/blue.mp3");
+      blueSound.play();
+      break;
+
+    default:
+      console.log("Unknown color");
+  }
 };

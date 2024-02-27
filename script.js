e254b5d02a82219fa4333b20b1a180c8a53abf15
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-27 16:43:50 +0200

    declare the colors array for the button colors

---
+++ script.js
@@ -0,0 +1,10 @@
+// TODO: Start the game by pressing any key
+// TODO: Create game pattern sequence
+// TODO: Store user's choices
+let colorChoices = ["green", "red", "yellow", "blue"];
+// let gamePattern = [];
+// let userChoice = [];
+
+// const randomGeneratedColor = function () {
+//   let randomColor = Math.floor(Math.random() * 4);
+// };
